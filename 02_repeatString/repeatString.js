4cff5fd3894e27e2c6891015c1ba1529f5179158
AlexJelani <jelanialexander82@gmail.com>
2022-03-19 15:37:40 +0900

    changed json packages

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
 
+const repeatString = function(string, times) {
+    //1. Create an empty string that will host the repeated string
+    var repeatedString = "";
+    //2. Set the while loop with (times > 0) as the condition to check
+    while (times > 0) {
+        //As long as times is greater than 0, the statement is executed
+        //The statement
+        repeatedString += string;//Same as repeatedString = repeatedString + string;
+        times++// Same as times = times + 1;
+    }
+    return repeatedString;
 };
-
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
