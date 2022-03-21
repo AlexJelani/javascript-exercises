a202492b1f677683cee38d7e452bf98ac4ed38de
AlexJelani <jelanialexander82@gmail.com>
2022-03-21 19:38:54 +0900

    Passed test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    //1. Use the split() method to return a new array
+    var splitString = str.split("");//var splitString = "hello".split("");
+    //["h","e","l","l","o"]
+
+    //2. Use the reverse() method to reverse the new created array
+    var reverseArray = splitString.reverse();//var reverse = ["h", "e", "l", "l", "o"].reverse();
+
+    // Step 3. Use the join() method to join all elements of the array into a string
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+     // "olleh"
+
+    //Step 4. Return the reversed string
+    return joinArray;
+
 
 };
-
+reverseString('hello there') // returns 'ereht olleh'
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, items) {
+  const idx = arr.indexOf(items);
+  arr.splice(idx, 1);
+  return arr;
 };
+removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
 
 // Do not edit below this line
 module.exports = removeFromArray;
