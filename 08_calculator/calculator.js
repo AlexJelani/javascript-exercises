c91cf012c8a96e8ac4b66c4b69a5c00be75d5327
AlexJelani <jelanialexander82@gmail.com>
2022-06-04 12:01:42 +0900

    updated calculator challenges

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -13,16 +13,25 @@ function sum(numbers){
   },0);
   return x;
 }
-const multiply = function(a,b) {
-  return a * b;
+
+
+const multiply = function(array) {
+	return array.length
+   ? array.reduce((accumlator, nextItem) => accumlator * nextItem)
+   :0;
+}
+
+const power = function(a, b) {
+	return Math.pow(a,b)
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
